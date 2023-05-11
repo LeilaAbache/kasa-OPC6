9133004516d153d5435c0f20d6dc0b3c5069cb1a
Leila Abache <leila.aba@hotmail.fr>
2023-05-11 22:09:47 +0200

    css

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,15 +3,19 @@ import { NavLink } from "react-router-dom";
 
 const Card = (logement) => {
   return (
-    <article className="card">
+    <article>
       <NavLink
         to={{
           pathname: "../fichelogement",
           search: "?_id=" + logement.card.id,
         }}
       >
-        <div className="card-image">
-          <img src={logement.card.cover} alt={logement.card.title} />
+        <div className="card">
+          <img
+            className="card-image"
+            src={logement.card.cover}
+            alt={logement.card.title}
+          />
         </div>
         <div className="card-name">
           <h2>{logement.card.title}</h2>
